55e1e5c49fca47bcc3742db64bbba0da78d4e189
AhmedNazem <aahhaammdd55@gmail.com>
2024-01-30 03:42:33 +0300

    create signup api route

--- api/controlers/user.controler.js
+++ api/controlers/user.controler.js
@@ -1,3 +1,4 @@
+//this file is the action of the user.route.js  most if the file conrollers work like that
 const test = (req, res) => {
   res.json({ message: "api tesitng :D" });
 };
--- api/index.js
+++ api/index.js
@@ -1,16 +1,18 @@
+//all router files are connected straght to this file but controllers are connected first with routes :D
 import mongoose from "mongoose";
 import express from "express";
 import dotenv from "dotenv";
 import UserRoute from "./routers/user.route.js";
-
+import authRouter from "./routers/auth.route.js";
 dotenv.config();
 mongoose
   .connect(process.env.MONGO)
   .then(() => console.log("successfully conected to database"))
   .catch((err) => console.log(err));
 const app = express();
-
+app.use(express.json());
 app.listen(3000, () => {
   console.log("app is running on port 3000 ");
 });
 app.use("/api/user", UserRoute);
+app.use("/api/auth", authRouter);
