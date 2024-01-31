08c8409e403028d65b2b01026816cc8fbd7607b8
AhmedNazem <aahhaammdd55@gmail.com>
2024-01-31 04:59:57 +0300

    create sign in api route

--- api/controlers/auth.controler.js
+++ api/controlers/auth.controler.js
@@ -1,5 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
+import { errorHandler } from "../utls/error.js";
+import jwt from "jsonwebtoken";
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
   const hashpassword = bcryptjs.hashSync(password, 10);
@@ -10,4 +12,23 @@ export const signup = async (req, res, next) => {
   } catch (error) {
     next(error);
   }
+};
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) return next(errorHandler(404, "user not found :("));
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) return next(errorHandler(401, "invalid password "));
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const { password: pass, ...rest } = validUser._doc;
+    // This technique is often used when you want to exclude certain properties
+    //from an object for security or privacy reasons,
+    // especially when sending data to clients or
+    // other parts of your application.
+    res
+      .cookie("access_token", token, { httpOnly: true })
+      .status(200)
+      .json(rest);
+  } catch (err) {}
 };
--- api/routers/auth.route.js
+++ api/routers/auth.route.js
@@ -1,6 +1,6 @@
 import express from "express";
-import { signup } from "../controlers/auth.controler.js";
+import { signin, signup } from "../controlers/auth.controler.js";
 const route = express.Router();
 route.post("/signup", signup);
-
+route.post("/signin", signin);
 export default route;
