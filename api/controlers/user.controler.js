6aa5fe38037cea550c5f5812233ca2387ec38706
AhmedNazem <aahhaammdd55@gmail.com>
2024-02-13 16:43:53 +0300

    retruning404inside isomnia err :(

--- api/controlers/user.controler.js
+++ api/controlers/user.controler.js
@@ -1,6 +1,34 @@
+import User from "../models/user.model.js";
+import { errorHandler } from "../utls/error.js";
+import bcryptjs from "bcryptjs";
 //this file is the action of the user.route.js  most if the file conrollers work like that
-const test = (req, res) => {
+export const test = (req, res) => {
   res.json({ message: "api tesitng :D" });
 };
 
-export default test;
+export const updateUser = async (req, res, next) => {
+  if (req.user.id !== req.params.id)
+    return next(errorHandler(401, "you can only update your own account "));
+  try {
+    if (req.body.password) {
+      req.body.password = bcryptjs.hashSync(req.body.password, 10);
+    }
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: {
+          //?preview the code
+          username: req.body.username,
+          email: req.body.email,
+          password: req.body.password,
+          avatar: req.body.avatar,
+        },
+      },
+      { new: true } //?also here :D
+    );
+    const { password, ...rest } = updatedUser._doc;
+    res.status(200).json(rest);
+  } catch (err) {
+    next(err);
+  }
+};
--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ import express from "express";
 import dotenv from "dotenv";
 import UserRoute from "./routers/user.route.js";
 import authRouter from "./routers/auth.route.js";
+import cookieParser from "cookie-parser";
 dotenv.config();
 mongoose
   .connect(process.env.MONGO)
@@ -11,12 +12,13 @@ mongoose
   .catch((err) => console.log(err));
 const app = express();
 app.use(express.json());
+app.use(cookieParser()); //? go some search about it
 app.listen(3000, () => {
   console.log("app is running on port 3000 ");
 });
 app.use("/api/user", UserRoute);
 app.use("/api/auth", authRouter);
-app.use((err, req, res, nest) => {
+app.use((err, req, res, next) => {
   //this is a middlleware
   const statusCode = err.statusCode || "500";
   const message = err.message || "internal server error ";
