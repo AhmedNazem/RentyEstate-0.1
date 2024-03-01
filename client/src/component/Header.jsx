ceb90f73e1a15ff6570e6a842ae60f7e35ec1ff4
AhmedNazem <aahhaammdd55@gmail.com>
2024-03-01 19:20:20 +0300

    complete search form functionality (partially ^_^)

--- client/src/component/Header.jsx
+++ client/src/component/Header.jsx
@@ -1,8 +1,30 @@
 import { FaSearch } from "react-icons/fa";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
+import { useEffect, useState } from "react";
+
 function Header() {
+  const navigate = useNavigate();
   const { currentUser } = useSelector((state) => state.user);
+  const [searchTerm, setSearchTerm] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const urlParams = new URLSearchParams(window.location.search);
+    urlParams.set("searchTerm", searchTerm);
+    const searchQuery = urlParams.toString();
+    navigate(`/search?${searchQuery}`);
+  };
+  //?we use the effect here so  when ever the user write somthing in the
+  //? link bar above beside the http:/localhost:3000 so we need to keep the track
+  //! when ever the user write somthing the url bar it changes in the search bar of the site
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const searchTermFromUrl = urlParams.get("searchTerm"); //* we take only the search term because in the url we have multible values such as offer , type etc.
+    if (searchTermFromUrl) {
+      //? if we get the info in the url set the same info in the site search bar
+      setSearchTerm(searchTermFromUrl);
+    }
+  }, [location.search]);
   return (
     <header className="bg-green-200 shadow-md ">
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
@@ -12,13 +34,20 @@ function Header() {
             <span className="text-green-800">Estate</span>
           </h1>
         </Link>
-        <form className="bg-green-100 p-3 rounded-lg flex items-center ">
+        <form
+          onSubmit={handleSubmit}
+          className="bg-green-100 p-3 rounded-lg flex items-center "
+        >
           <input
             type="text"
             placeholder="Search..."
             className="bg-transparent focus:outline-none w-24 sm:w-64 "
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
           />
-          <FaSearch className="text-green-700" />
+          <button>
+            <FaSearch className="text-green-700" />
+          </button>
         </form>
         <ul className="flex gap-4">
           <Link to="/">
