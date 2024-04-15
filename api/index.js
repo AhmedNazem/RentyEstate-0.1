a9f2db21719247a8f6b0dce74df74aee5652ab3a
AhmedNazem <aahhaammdd55@gmail.com>
2024-04-15 21:09:55 +0300

    intializing the netlify hosting server

--- api/index.js
+++ api/index.js
@@ -6,11 +6,13 @@ import UserRoute from "./routers/user.route.js";
 import authRouter from "./routers/auth.route.js";
 import cookieParser from "cookie-parser";
 import listingRouter from "./routers/listing.route.js";
+import path from "path";
 dotenv.config();
 mongoose
   .connect(process.env.MONGO)
   .then(() => console.log("successfully conected to database"))
   .catch((err) => console.log(err));
+const _dirname = path.resolve();
 const app = express();
 app.use(express.json());
 app.use(cookieParser()); //? go some search about it
@@ -20,6 +22,13 @@ app.listen(3000, () => {
 app.use("/api/user", UserRoute);
 app.use("/api/auth", authRouter);
 app.use("/api/listing", listingRouter);
+
+app.use(express.static(path.join(_dirname, "/client/dist")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(_dirname, "client", "dist", "index.html"));
+});
+
+//* creating a moddleware
 app.use((err, req, res, next) => {
   //this is a middlleware
   const statusCode = err.statusCode || "500";
